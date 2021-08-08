91c431230e4cf67666fb727d3aa8d71baa4a3957
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-08 14:19:57 +0600

    task3

---
+++ task3.js
@@ -0,0 +1,24 @@
+var num1 = 5;
+var num2 = 3;
+var result1 = num1 + num2;
+console.log(result1);
+
+var num1 = 5;
+var num2 = 3;
+var result1 = num1 - num2;
+console.log(result1);
+
+var num1 = 5;
+var num2 = 3;
+var result1 = num1 * num2;
+console.log(result1);
+
+var num1 = 6;
+var num2 = 3;
+var result1 = num1 / num2;
+console.log(result1);
+
+var num1 = 9;
+var num2 = 3;
+var result1 = num1 % num2;
+console.log(result1);
