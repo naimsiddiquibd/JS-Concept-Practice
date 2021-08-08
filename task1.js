eca6f9a60ceb6065bbcc5e05482e857348182f74
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-08-08 13:09:45 +0600

    first commit

---
+++ task1.js
@@ -0,0 +1,6 @@
+// write 3 variables (number, string boolean)
+var myAge = 20;
+var myName = 'Naim Siddiqui';
+var isEngineer = true;
+console.log('My name is ', myName , 'I am ', myAge , 'years old');
+console.log(isEngineer);
